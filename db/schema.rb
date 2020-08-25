b1106ed33552f0779a37ed14c512acad066e7a2d
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2020-08-25 15:00:17 +0100

    db changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_162707) do
+ActiveRecord::Schema.define(version: 2020_08_25_134752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_08_24_162707) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "finance_provider_id"
+    t.string "colour"
     t.index ["finance_provider_id"], name: "index_cars_on_finance_provider_id"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
@@ -59,6 +60,9 @@ ActiveRecord::Schema.define(version: 2020_08_24_162707) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
