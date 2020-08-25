4575c0d4c6b6f745c655789b9e5941c0a375a058
Haseeb Zia <haseebzia99@outlook.com>
2020-08-25 17:48:29 +0100

    banner/search bar backend

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if params[:query].present?
+     @cars = Car.search_by_make_and_model(params[:query])
+    else
+      @cars = Car.all
+    end
   end
+
 end
