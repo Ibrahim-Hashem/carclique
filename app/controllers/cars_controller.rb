5855027b933e02635f87eee37bd8d0f0d2e0c1ed
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2020-08-26 12:12:47 +0100

    seed added

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,12 +2,11 @@ class CarsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @cars = Car.all
+    @cars_all = Car.all
     if !params[:search].nil?
       @cars = Car.where(make: params[:search][:query_make]).where(model: params[:search][:query_model])
     else
       @cars = Car.all
     end
-
   end
 end
