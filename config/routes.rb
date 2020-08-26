a72fa5da8097123ff380762139fa4c5015308592
Deep Biswas <deepbiswas@hotmail.com>
2020-08-26 16:02:56 +0100

    The-bidding-system

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'transactions/create'
+  get 'transactions/update'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cars, only: [:show]
+  resources :cars, only: [:show] do
+    resources :transactions, only: [:create]
+  end
 end
