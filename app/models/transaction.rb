ce5d3bb7be413afa016a3bdb16655fa14906827f
Deep Biswas <deepbiswas@hotmail.com>
2020-08-27 09:09:28 +0100

    frontendchanges

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,15 @@
 class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :car
-  validates :amount, numericality: { greater_than_or_equal_to: 0, greater_than: Transaction.where(car: car).last.amount }
+  # validates :amount, numericality: { greater_than_or_equal_to: 0, greater_than: Transaction.where(car: Self.car).last.amount }
+  validate :minimum_amount
+  
+  private 
+
+  def minimum_amount
+    if Transaction.where(car: self.car).order(created_at: :asc).last.amount >= self.amount
+      errors.add(:amount, "must be greater than the previous bid")
+    end
+  end
   #the above where statement is referring to the car which is specific to that current bid/transaction.
 end
