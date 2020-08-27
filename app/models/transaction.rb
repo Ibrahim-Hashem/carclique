a89605af58115236d7b2d86e05110ef0f19e8613
Haseeb Zia <haseebzia99@outlook.com>
2020-08-27 17:02:58 +0100

    geocoding back/front end

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :car
-  validates :amount, numericality: { greater_than_or_equal_to: 0, greater_than: Transaction.where(car: car).last.amount }
+  validates :amount, numericality: { greater_than_or_equal_to: 0, greater_than: Transaction.where(car: self.car).last.amount }
   #the above where statement is referring to the car which is specific to that current bid/transaction.
 end
